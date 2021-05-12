15d8aeb6791758b87afd7f3e62b98fd72f659862
Vladislav <vl.ivaschenkoo@gmail.com>
2021-05-12 21:32:54 +0300

    setting up the working environment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,14 @@ const { responseMiddleware } = require('../middlewares/response.middleware');
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.post('/users', (req, res, next) => {
+  try {
+    console.log(JSON.stringify(req));
+  } catch (err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
 
 module.exports = router;
