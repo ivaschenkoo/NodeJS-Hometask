2002ab8f119e0084470f6af3bfa2d3d15617854c
Vladislav <vl.ivaschenkoo@gmail.com>
2021-05-14 01:19:37 +0300

    added build for heroku

---
+++ client/build/precache-manifest.73eb209887a5b407e3f0f2a22111b41a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f20068d810b1bda3382b0c911792c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fec67e7461dcd0971a08",
+    "url": "/static/css/main.d81c9a72.chunk.css"
+  },
+  {
+    "revision": "a9d587b0dbc91298188c",
+    "url": "/static/js/2.456e748c.chunk.js"
+  },
+  {
+    "revision": "e45695bb30c26ce6af8405cb08c7f90a",
+    "url": "/static/js/2.456e748c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec67e7461dcd0971a08",
+    "url": "/static/js/main.ec3b68a8.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
